832962f3253b772c90a55d6ecc72e7758a671740
KAMAl324132 <drissielbouzaidikamal2@gmail.com>
2025-05-21 16:56:09 +0200

    Remove unnecessary .js files

--- week1/day1/daily-challenges/exercice/exercice 7.js
+++
@@ -1,8 +0,0 @@
-const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
-
-let societyName = '';
-names.forEach(name => {
-  societyName += name[0];  
-});
-
-console.log(societyName.split('').sort().join(''));
